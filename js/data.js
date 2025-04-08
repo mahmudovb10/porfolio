49cdb286a5ebd510ab1dc09a9a31e90aec5eae95
bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-04-08 22:36:52 +0500

    My-Portfolio

---
+++ js/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: "Intro Section",
+    github: "https://github.com/mahmudovb10/Intro-Section-Project",
+    vercel: "https://vercel.com/mahmudovb10s-projects/intro-section-project",
+  },
+  {
+    title: "Card 2",
+    github: "https://figma-change.vercel.app/",
+    vercel: "https://vercel.com/mahmudovb10s-projects/figma-change",
+  },
+  {
+    title: "Card 1",
+    github: "https://github.com/mahmudovb10/7-Dars",
+    vercel: "https://vercel.com/mahmudovb10s-projects/card-w",
+  },
+];
