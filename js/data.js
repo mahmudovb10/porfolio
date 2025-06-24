bae8c5f76c84eb1b0d4d47d5bedd81cbbcf9e311
bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-06-24 14:42:39 +0500

    update data.js

--- js/data.js
+++ js/data.js
@@ -1,95 +1,101 @@
 export default [
-                                          {
+  {
+    title: "Music Player",
+    github: "https://github.com/mahmudovb10/Music-Player",
+    vercel: "music-player-wine-theta.vercel.app",
+  },
+  {
     title: "Sunny Side (Imtihon-2)",
     github: "https://github.com/mahmudovb10/imtihon-2",
     vercel: "https://imtihon-2-tau.vercel.app/",
   },
-                                        {
+  {
     title: "insure (Imtihon-1)",
     github: "https://github.com/mahmudovb10/imtihon-1",
     vercel: "https://imtihon-1-six.vercel.app/",
   },
-                                        {
+  {
     title: "John",
     github: "https://github.com/mahmudovb10/John",
     vercel: "https://john-1w6c.vercel.app/",
   },
-                                    {
+  {
     title: "News Homepage",
     github: "https://github.com/mahmudovb10/News-Homepage",
     vercel: "https://news-homepage-iota-silk.vercel.app/",
   },
-                                      {
+  {
     title: "Intro Component",
     github: "https://github.com/mahmudovb10/Intro-Component",
     vercel: "https://intro-component-jet.vercel.app/",
   },
-                                    {
+  {
     title: "Product Preview",
     github: "https://github.com/mahmudovb10/Product-Preview",
     vercel: "https://product-preview-three-beta.vercel.app/",
   },
-                                {
+  {
     title: "Single Page (Responsive Added)",
     github: "https://github.com/mahmudovb10/single-pagee",
     vercel: "https://vercel.com/mahmudovb10s-projects/single-pagee",
   },
-                              {
+  {
     title: "Tech Book",
     github: "https://github.com/mahmudovb10/tech-books",
     vercel: "https://vercel.com/mahmudovb10s-projects/tech-books",
   },
-                            {
+  {
     title: "Bento Grid (Incomplete)",
     github: "https://grid-bento.vercel.app/",
     vercel: "https://grid-bento.vercel.app/",
   },
-                          {
+  {
     title: "Testemonials Grid",
     github: "https://grid-card-khaki.vercel.app/",
     vercel: "https://grid-card-khaki.vercel.app/",
   },
-                        {
+  {
     title: "Namanganliklar 24",
     github: "https://scss-templete-opal.vercel.app/",
     vercel: "https://vercel.com/mahmudovb10s-projects/scss-templete",
   },
-                      {
+  {
     title: "Single Page",
     github: "https://single-page-indol.vercel.app/",
     vercel: "https://single-page-indol.vercel.app/",
   },
-                    {
+  {
     title: "Animation",
     github: "https://github.com/mahmudovb10/Animation",
     vercel: "https://vercel.com/mahmudovb10s-projects/animation",
   },
-                  {
+  {
     title: "Coffee Roaster",
     github: "https://github.com/mahmudovb10/coffee-roasters",
     vercel: "https://vercel.com/mahmudovb10s-projects/coffee-roasters",
   },
-                  {
+  {
     title: "My Team Finished",
     github: "https://github.com/mahmudovb10/My-Team-Finished",
     vercel: "https://vercel.com/mahmudovb10s-projects/my-team-finished",
   },
-         {
+  {
     title: "My Team",
     github: "https://github.com/mahmudovb10/My-Team-figma",
     vercel: "https://vercel.com/mahmudovb10s-projects/my-team-figma-1gks",
   },
-        {
+  {
     title: "Intro Section",
     github: "https://github.com/mahmudovb10/Intro-Section-Project",
-    vercel: "https://vercel.com/mahmudovb10s-projects/intro-section-project-86ab",
+    vercel:
+      "https://vercel.com/mahmudovb10s-projects/intro-section-project-86ab",
   },
-      {
+  {
     title: "Contact",
     github: "https://github.com/mahmudovb10/Contact",
     vercel: "https://vercel.com/mahmudovb10s-projects/contact",
   },
-    {
+  {
     title: "Recipe",
     github: "https://recipe-page-puce-pi.vercel.app/",
     vercel: "https://vercel.com/mahmudovb10s-projects/recipe-page",
